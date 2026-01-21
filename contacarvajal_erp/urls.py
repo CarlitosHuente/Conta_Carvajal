6aa083c0830dbd663c58ead8c6045fc3c207e5e3
Carlos Carvajal <carloscarvajal2.0@gmail.com>
2026-01-21 00:01:31 -0300

    Pagina Principal

--- contacarvajal_erp/urls.py
+++ contacarvajal_erp/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView  # <--- Agregar esto
+from django.views.generic import TemplateView # <--- Necesario para la portada simple
 
 urlpatterns = [
-    # Esta es la línea mágica que arregla el 404:
+    # Portada (Home)
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     
+    # Admin
     path('admin/', admin.site.urls),
+    
+    # Tus aplicaciones
     path('rrhh/', include('rrhh.urls')),
 ]
